85842bc3925a7dcf937b17f0306d60ca665c2619
NurislamBejsebaev <130899878+NurislamBejsebaev@users.noreply.github.com>
2023-10-04 16:14:07 +0600

    'added_jwt_test'

--- collection/tests.py
+++ collection/tests.py
@@ -1,5 +1,10 @@
+import json
+from rest_framework.test import APIClient
 from rest_framework.test import APITestCase
+from rest_framework.authtoken.models import Token
+from django.contrib.auth.models import User
 from .factories import CollectionFactory
+from usersapp.factories import UserFactory
 from .models import GameCollection
 
 
@@ -8,46 +13,76 @@ class CollectionsTest(APITestCase):
         self.col_1 = CollectionFactory()
         self.col_2 = CollectionFactory()
         self.col_3 = CollectionFactory()
+        self.client = APIClient()
 
     def test_get_list_of_3_collections(self):
+        user = UserFactory()
+        token, _ = Token.objects.get_or_create(user=user)
+        self.col_1 = CollectionFactory(name='Test collection 7')
+        self.col_2 = CollectionFactory(name='Test collection 8')
+        self.col_3 = CollectionFactory(name='Test collection 9')
+        self.client.credentials(HTTP_AUTHORIZATION=f'Token {token.key}')
         response = self.client.get('/collections/')
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(self.col_1.name, response.data[0]["name"])
-        self.assertEqual(self.col_2.name, response.data[1]["name"])
-        self.assertEqual(self.col_3.name, response.data[2]["name"])
+        collections = response.data['results']
+        self.assertEqual(self.col_1.name, collections[0]["name"])
+        self.assertEqual(self.col_2.name, collections[1]["name"])
+        self.assertEqual(self.col_3.name, collections[2]["name"])
 
     def test_get_one_collection(self):
+        user = UserFactory()
+        token, _ = Token.objects.get_or_create(user=user)
+
+        # Создаем коллекцию
+        self.col_1 = CollectionFactory()
+        self.client.credentials(HTTP_AUTHORIZATION=f'Token {token.key}')
         response = self.client.get(f'/collections/{self.col_1.pk}/')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(self.col_1.name, response.data["name"])
 
     def test_create_collection(self):
+        user = UserFactory()
+        token, _ = Token.objects.get_or_create(user=user)
+        self.client.credentials(HTTP_AUTHORIZATION=f'Token {token.key}')
         collection_data = {
             'name': 'New collection',
-            'likes': [
-                1
-                      ],
+            'likes': [1],
             'author': 1
         }
-        response = self.client.post('/collections/', collection_data)
-        self.assertEqual(response.status_code, 201)  # Ожидаем статус 201, так как это создание ресурса
-        self.assertEqual(collection_data['name'], response.data["name"])
+        response = self.client.post('/collections/', json.dumps(collection_data), content_type='application/json')
+        self.assertEqual(response.status_code, 201)
 
     def test_update_collection(self):
+        user = UserFactory(pk=1)  # Указываем первичный ключ 1
+        token, _ = Token.objects.get_or_create(user=user)
+        self.col_1 = CollectionFactory()
+        self.client.credentials(HTTP_AUTHORIZATION=f'Token {token.key}')
         updated_name = 'New Collection Name'
         updated_data = {
             'name': updated_name,
-            'likes': [14],
-            'author': 14
+            'likes': [1],  # Используем корректные первичные ключи лайков
+            'author': user.pk  # Используем корректный первичный ключ автора
         }
         response = self.client.put(f'/collections/{self.col_1.pk}/', updated_data)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(updated_name, response.data["name"])
 
+
     def test_delete_collection(self):
+        user = UserFactory()
+        token, _ = Token.objects.get_or_create(user=user)
+        self.col_1 = CollectionFactory()
+        self.client.credentials(HTTP_AUTHORIZATION=f'Token {token.key}')
         response = self.client.delete(f'/collections/{self.col_1.pk}/')
         self.assertEqual(response.status_code, 204)
-        self.assertEqual(GameCollection.objects.count(), 2)
+        self.assertEqual(GameCollection.objects.count(), 3)
+
+
+
+
+
+
+
 
 
 
--- games/tests.py
+++ games/tests.py
@@ -1,3 +1,7 @@
+from rest_framework.test import APIClient
+from rest_framework.authtoken.models import Token
+from usersapp.factories import UserFactory
+from django.contrib.auth.models import User
 from rest_framework.test import APITestCase
 from .factories import *
 
@@ -7,19 +11,33 @@ class GamesTest(APITestCase):
         self.game_1 = GameFactory(year=2024)
         self.game_2 = GameFactory(year=2024)
         self.genre = GenreFactory()
+        self.client = APIClient()
 
     def test_get_list_of_games(self):
         response = self.client.get('/games/list-create/')
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 302)
 
 
 
     def test_get_one_game(self):
+        user = UserFactory()
+        token, _ = Token.objects.get_or_create(user=user)
+        self.genre = GenreFactory()
+        self.client.credentials(HTTP_AUTHORIZATION=f'Token {token.key}')
         response = self.client.get(f'/games/games/{self.game_1.pk}/')
         self.assertEqual(response.status_code, 200)
 
-
-
     def test_get_genre(self):
+        user = UserFactory()
+        token, _ = Token.objects.get_or_create(user=user)
+        self.genre = GenreFactory()
+        self.client.credentials(HTTP_AUTHORIZATION=f'Token {token.key}')
         response = self.client.get(f'/genre/{self.genre.pk}/')
         self.assertEqual(response.status_code, 200)
+
+
+
+
+
+
+
